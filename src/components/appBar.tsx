9017382aff6778a2982c84bbf634d7760f5a8f7e
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-01-01 16:19:33 -0800

    fix link to dex

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -43,7 +43,7 @@ export const AppBar = (props: { left?: JSX.Element; right?: JSX.Element }) => {
           </Menu.Item>
           <Menu.Item key="trade">
             <a
-              href={"https://dex.projectserum.com"}
+              href={"https://openserum.ch/dex/"}
               target="_blank"
               rel="noopener noreferrer"
             >
